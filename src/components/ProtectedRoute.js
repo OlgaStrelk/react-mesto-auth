8928aa0057167ed4729306368f9a41c353234f17
Strelkova Olga <strelod@yandex.ru>
2022-06-03 23:41:13 +0300

    feat add protected route

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,8 +1,12 @@
-import React from "react";
-function ProtectedRoute() {
-    return (
-<></>
-    )
+import React from 'react';
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({ loggedIn, children, ...props }) => {
+  return (
+      <Route {...props}>
+        {loggedIn ? children : <Redirect to="/login"/>}
+      </Route>
+  )
 }
 
 export default ProtectedRoute;
